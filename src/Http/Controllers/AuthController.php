db0679eaa1cf7bd895df09e06531ae27af8301ee
eiixy <eiixy@zbanx.com>
2021-07-26 15:15:30 +0800

    增加账号密码登录方式

--- src/Http/Controllers/AuthController.php
+++ src/Http/Controllers/AuthController.php
@@ -18,17 +18,11 @@ class AuthController extends Controller
 
     public function login(Request $request): \Illuminate\Http\JsonResponse
     {
-        $ticket = $request->input('ticket');
-
-        try {
-            $client = new HttpClient();
-            $response = $client->request('GET', "/api/tickets/{$ticket}");
-        } catch (GuzzleException $exception) {
-            return $this->error($exception->getMessage(), -2);
-        } catch (CasClientException $exception) {
-            return $this->error($exception->getMessage(), -3);
+        if ($request->filled('ticket')) {
+            $response = $this->loginWithTicket($request->input('ticket'));
+        } elseif ($request->filled('account') && $request->filled('password')) {
+            $response = $this->loginWithPassword($request->all(['account', 'password']));
         }
-
         if ($response['code'] != 0) {
             return $this->error($response['message'], -1);
         }
@@ -46,6 +40,7 @@ class AuthController extends Controller
         $token = auth($auth['guard'])->login($user);
 
         CasCache::setPermissions($response['data']['account_id'], $response['data']['permissions']);
+        $ticket = $request->filled('ticket') ? $request->get('ticket') : $response['data']['ticket'];
         CasCache::setUserTicket($response['data']['account_id'], $ticket);
 
         return $this->success([
@@ -64,13 +59,44 @@ class AuthController extends Controller
         auth($guard)->logout();
         CasCache::delPermissions($user_id);
 
-        try {
-            $client = new HttpClient();
-            $client->request('DELETE', "/api/tickets/{$ticket}");
-        } catch (CasClientException | GuzzleException $e) {
+        if (!empty($ticket)) {
+            try {
+                $client = new HttpClient();
+                $client->request('DELETE', "/api/tickets/{$ticket}");
+            } catch (CasClientException | GuzzleException $e) {
 
+            }
         }
 
         return $this->success();
+    }
+
+    private function loginWithTicket($ticket)
+    {
+        try {
+            $client = new HttpClient();
+            $response = $client->request('GET', "/api/tickets/{$ticket}");
+        } catch (GuzzleException $exception) {
+            return $this->error($exception->getMessage(), -2);
+        } catch (CasClientException $exception) {
+            return $this->error($exception->getMessage(), -3);
+        }
+
+        return $response;
+    }
+
+    private function loginWithPassword($data)
+    {
+        try {
+            $client = new HttpClient();
+            $response = $client->request('POST', "/api/login", [
+                'body' => $data
+            ]);
+        } catch (GuzzleException $exception) {
+            return $this->error($exception->getMessage(), -2);
+        } catch (CasClientException $exception) {
+            return $this->error($exception->getMessage(), -3);
+        }
+        return $response;
     }
 }
